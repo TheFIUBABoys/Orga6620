9fba0cdece7180d4747f40ba42873ca06440c761
Lucas <lucas@lucas-VM-Mint.(none)>
2013-11-17 17:46:49 -0300

    Bases tp1

---
+++ tp1/reverse.h
@@ -0,0 +1,20 @@
+#ifndef REVERSE_H
+#define REVERSE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+
+#define TAM_INI_CADENA 40
+
+extern const char* reverse_errmsg[]
+
+
+
+
+int reverse(int infd, int outfd)
+
+#endif
