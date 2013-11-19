6c53dd4bb53a3e8bd2b77f2b7ff2d54f5d31df1b
tomasboccardo <tomasboccardo@gmail.com>
2013-11-19 16:44:32 -0300

    Pruebas del realloc

--- tp1/sys_mmap/main.c
+++ tp1/sys_mmap/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <sys/types.h>
-#include <mymalloc.h>
+#include "mymalloc.h"
 
 int main(int argc, char * const argv[]){
 	size_t i;
@@ -16,8 +16,30 @@ int main(int argc, char * const argv[]){
 		printf(" Writing ...");
 		fflush(stdout);
 		for (j = 0; j < i; ++j)
-			p[j] = 0xff;
+			p[j] = 0x10;
 		printf(" Ok.");
+		
+		fprintf(stdout, "myrealloc(%p,%ld) ...",p, (long) i*2);
+		fflush(stdout);
+		p = (char *) myrealloc(p,i*2);
+		fprintf(stdout, " %p.", p);
+		printf(" Checking...");
+		fflush(stdout);
+		int error = 1;
+		for (j = 0; j < i; ++j){
+			if (p[j] != 0x10)
+				error = 0;
+		}
+		if (error != 0)
+			printf(" Ok.");
+		else
+			printf(" Error.");
+			
+		printf(" Writing ...");
+		fflush(stdout);
+		for (j = i; j < i*2; ++j)
+			p[j] = 0x10;
+		printf(" Ok.");	
 
 		printf(" Freeing memory ...");
 		fflush(stdout);
